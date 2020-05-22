5ab5481140bbd11a266ec054f4a7bb9ee504ddbd
Julian Goldsmith <julian@juliangoldsmith.com>
2020-05-22 11:58:05 -0400

    RLE: Improve usage of pattern matching in decode.

--- rle.erl
+++ rle.erl
@@ -22,20 +22,16 @@ encode(<<HByte, TBytes/binary>>) -> encode(TBytes, HByte, 1, <<>>);
 encode(<<>>) -> <<>>.
 
 
-% PCount of 0 means we need to initialize the run.
-% That is required because we don't always have a next byte.
-decode(<<CByte, TBytes/binary>>, _, 0, Acc) ->
-	decode(TBytes, CByte, 1, Acc);
-decode(<<CByte, TBytes/binary>>, PByte, 4, Acc) ->
-	Expanded = binary:copy(<<PByte>>, 4 + CByte),
-	decode(TBytes, 0, 0, <<Acc/binary, Expanded/binary>>);
-decode(<<CByte, TBytes/binary>>, CByte, PCount, Acc) ->
-	decode(TBytes, CByte, PCount + 1, Acc);
-decode(<<CByte, TBytes/binary>>, PByte, PCount, Acc) -> 
-	Expanded = binary:copy(<<PByte>>, PCount),
-	decode(TBytes, CByte, 1, <<Acc/binary, Expanded/binary>>);
-decode(<<>>, PByte, PCount, Acc) ->
-	Expanded = binary:copy(<<PByte>>, PCount),
-	<<Acc/binary, Expanded/binary>>.
+% Manually matching is simpler and more concise than counting.
+decode(<<CByte, CByte, CByte, CByte, CCount, TBytes/binary>>, Acc) ->
+	Expanded = binary:copy(<<CByte>>, 4 + CCount),
+	decode(TBytes, <<Acc/binary, Expanded/binary>>);
+decode(<<CByte, CByte, CByte, TBytes/binary>>, Acc) ->
+	decode(TBytes, <<Acc/binary, CByte, CByte, CByte>>);
+decode(<<CByte, CByte, TBytes/binary>>, Acc) ->
+	decode(TBytes, <<Acc/binary, CByte, CByte>>);
+decode(<<CByte, TBytes/binary>>, Acc) ->
+	decode(TBytes, <<Acc/binary, CByte>>);
+decode(<<>>, Acc) -> Acc.
 
-decode(Bytes) -> decode(Bytes, 0, 0, <<>>).
+decode(Bytes) -> decode(Bytes, <<>>).
